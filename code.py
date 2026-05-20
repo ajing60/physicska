e276ff51e8149e67c967660a131619bba31b13f6
ajing60 <audrey.y.jing@gmail.com>
2026-05-20 18:04:08 +0000

    add

--- code.py
+++ code.py
@@ -7,3 +7,17 @@ d_theta = pi/100
 while (theta <= 2*pi):
     theta += d_theta
     coaster.append (3*vector (cos(theta),sin(theta),0))
+
+cart = sphere (pos=coaster.point(0).pos+vector(0.1,color=color.red,radius=0.1,speed=0,mass=1))
+g=1
+
+def track(posn):
+    x=posn.x
+    z=posn.z
+    y=0
+    slope = 0
+    displacement = vector (0,0,0)
+#find pos along track
+    for i in range (coaster.npoints-1):
+        if (coaster.point(i).pos.x <= x <= coaster.point(i+1).pos.x and coaster.point(i).pos.z <= z <= coaster.point(i+1).pos.z):
+            slope = (coaster.point(i+1).pos.y-coaster.point(i).pos.y)/(coaster.point(i+1).pos.x-coaster.point(i).pos.x)
