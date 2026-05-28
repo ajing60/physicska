40499dd0be51884dc1a7cc680e22179cfc816d9f
ajing60 <audrey.y.jing@gmail.com>
2026-05-28 18:04:54 +0000

    add

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ GlowScript 3.0 VPython
 coaster = curve (vector (-2, 5, 0), vector (-1, 3, 0), vector (0, 2, 0), vector (1, 2, 0), vector(2, 3, 0), vector (4,6,0))
 
 theta = pi
-d_theta = pi/100
+d_theta = pi/git100
 while (theta <= 2*pi):
     theta += d_theta
     coaster.append (3*vector (cos(theta),sin(theta),0))
