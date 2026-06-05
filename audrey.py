42c9b3d7e4c47c30b168f746235e4affbd3b1040
ajing60 <audrey.y.jing@gmail.com>
2026-06-05 17:36:20 +0000

    im not sure the friction is actually doing anything

--- audrey.py
+++ audrey.py
@@ -1,101 +1,181 @@
-GlowScript 2.7 VPython
-#Call the library for Motion Map
-get_library('https://rawgit.com/perlatmsu/physutil/master/js/physutil.js')
- 
-#Window setup
-scene.range = 15
-scene.background = vector(1, 1, 0.8)
- 
- 
-#the values below can be adjusted to change the properties of the ramp
-ramp_angle = 23 * pi/180
-ramp_depth = 6
-ramp_width = 30
-ramp_origin = vec(-15, -10, 0)
- 
-#the code below draws the ramp (lines 17 - 45)
-ramp_color = color.gray(0.3)
-edge_thickness = 0.05
-edge_color = color.black
- 
-a = vertex(pos=vec(0, 0, 0)+ramp_origin, color=ramp_color, opacity=.5)
-b = vertex(pos=vec(0, 0, ramp_depth)+ramp_origin, color=ramp_color, opacity=.5)
-c = vertex(pos=vec(ramp_width, 0, 0)+ramp_origin, color=ramp_color, opacity=.5)
-d = vertex(pos=vec(ramp_width, 0, ramp_depth)+ramp_origin, color=ramp_color, opacity=.5)
-e = vertex(pos=vec(0, ramp_width * tan(ramp_angle), 0)+ramp_origin, color=ramp_color, opacity=.5)
-f = vertex(pos=vec(0, ramp_width * tan(ramp_angle), ramp_depth)+ramp_origin, color=ramp_color, opacity=.5)
- 
-ramp_base = quad(v0=a, v1=b, v2=d, v3=c)
-ramp_surface = quad(v0=e, v1=f, v2=d, v3=c)
-ramp_back = quad(v0=e, v1=f, v2=b, v3=a)
-ramp_side_near = triangle(vs=[a, c, e])
-ramp_side_far = triangle(vs=[b, d, f])
- 
-def edge(v1, v2, r):
-    cylinder(pos = v1.pos, axis = v2.pos-v1.pos, radius = r, color = edge_color)
- 
-ab = edge(a, b, edge_thickness)
-ac = edge(a, c, edge_thickness)
-bd = edge(b, d, edge_thickness)
-cd = edge(c, d, edge_thickness)
-ae = edge(a, e, edge_thickness)
-bf = edge(b, f, edge_thickness)
-ef = edge(e, f, edge_thickness)
-df = edge(d, f, edge_thickness)
-ec = edge(e, c, edge_thickness)
- 
- 
-#The values below can be changed to modify the dimensions of the block
-block_width = 3
-block_height = 3
-block_depth = 3
- 
-#Create the block on the ramp
-block = box( pos=vector(0.5*(block_width**2+block_height**2)**0.5 * cos(atan(block_height/block_width) - ramp_angle), ramp_width * tan(ramp_angle) + 0.5*(block_width**2+block_height**2)**0.5 * sin(atan(block_height/block_width) - ramp_angle), 0.5*ramp_depth) + ramp_origin, 
-            axis=vector(ramp_width, -ramp_width * tan(ramp_angle), 0), 
-            size=vector(block_width,block_height,block_depth),
-            color = vector(0.65, 0.15, 0.15),
-            texture = textures.wood_old,
-            opacity = 0.7)
- 
- 
-#Set up the physical properties like the mass and velocity of the block, the acceleration due to gravity, time and the coefficient of friction.
-mblock = 50
-vblock = vector(0, 0, 0)
-g = vector(0,-9.8,0)
-cof = 0.3   #coefficient of friction#
+Web VPython 3.2
+#define size of viewing box
+x_min = -80
+x_max = 80
+fps = 100
 t = 0
-tf = 5
-dt = .001
- 
-#Define the force vectors acting on the block, including kinetic friction
-Fgrav = mblock * g
-Fnorm = vector(mag(Fgrav) * cos(ramp_angle)*sin(ramp_angle), mag(Fgrav)*cos(ramp_angle)*cos(ramp_angle), 0)
-Ffr = vector(-cof*mag(Fnorm)*cos(ramp_angle), cof*mag(Fnorm)*sin(ramp_angle), 0) 
-Fnet = Fgrav + Fnorm + Ffr
- 
-#Create arrows to show the forces with labels
-FgravArrow = arrow(pos = block.pos, axis=Fgrav/mblock, shaftwidth=0.3, color = color.green)
-FnormArrow = arrow(pos = block.pos, axis=Fnorm/mblock, shaftwidth=0.3, color = color.green)
-FfrArrow = arrow(pos = block.pos, axis=Ffr/mblock, shaftwidth=0.3, color = color.green)
-FgravLabel = label(pos=FgravArrow.pos, text='Fg', xoffset=-20, yoffset=-50, space=30, height=16, border=4, font='sans', line=False, color = color.black)
-FnormLabel = label(pos=FnormArrow.pos, text='Fn', xoffset=-10, yoffset=70, space=30, height=16, border=4, font='sans', line=False, color = color.black)
-FfrLabel = label(pos=FfrArrow.pos, text='Ffr', xoffset=-30, yoffset=30, space=30, height=16, border=4, font='sans', line=False, color = color.black)
- 
- 
-#Slide the block (and arrows) down the ramp until it reaches the end of the ramp
-while block.pos.y > ( ramp_origin.y+ 0.5*(block_width**2+block_height**2)**0.5 * cos(atan(block_width/block_height) - ramp_angle) ):
-    rate(500)
- 
- 
-    block.pos = block.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2 
- 
-    FgravArrow.pos = FgravArrow.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
-    FnormArrow.pos = FnormArrow.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
-    FfrArrow.pos = FfrArrow.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
-    FgravLabel.pos = FgravLabel.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
-    FnormLabel.pos = FnormLabel.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
-    FfrLabel.pos = FfrLabel.pos + vblock*dt + 0.5*(Fnet/mblock)*dt**2
- 
-    vblock = vblock + (Fnet/mblock)*dt
-    t = t + dt 
+dt = 1/fps
+g = 9.81
+Fg = vector (0, -g, 0)
+#making buttons for path type
+path_type = "curves"#"ramp""curves" #loop
+mass= 10
+path_curve = None
+#marble_r=4
+marble_r=pow(mass, 1/3)
+k = 0.1
+
+def friction(myevt):
+    global k
+    k =myevt.value
+    
+myslider = slider( bind=friction, min=0, max=1, length =200 )
+
+
+    
+def path (x):
+    if path_type == "ramp": 
+        if x_min<x < 0: return 10
+        if 0 <= x <= 30: return 10 - (0.5 * x)
+        return -50
+    elif path_type == "curves":
+        if  x_min<= x <= x_max: 
+            return 4 * sin (x/10)
+        else: return -50
+    
+    #elif path_type == "loop":
+    #    if (-70 <= x <=70): 
+    #        return ((225 - (x-5)**2)**0.5)+1.7
+        #if (-70 <= x <=70 && marble.pos.y <= 0):
+        #    return -((225 - (x-5)**2)**0.5)+1.7
+        #elif (1.7 < marble.pos.y): 
+        #    if (4 <= x <= 6): 
+        #        return -1 * ((1 - (x-5)**2)**0.5)+1.7
+        #elif 0 <= x <= 5: 
+        #    return 0.5 * exp(x+5)
+        #elif 5 <= x <= 10: 
+        #    return 0.5 * exp(-x + 5)
+    #    else:
+    #        return -50
+    elif path_type == "loop":
+        if  x_min<=x<0:
+            return 0.03*(x+20)**2-40
+        #elif 0<=x<=80:
+        #    return -29
+        
+        '''
+        if (0.9 <= marble.pos.y <= 1.7): 
+            if (4 <= x <= 4.4 or 5.6 <= x <= 6): 
+                return ((1 - (x-5)**2)**0.5)+1.7
+            elif (1.7 < marble.pos.y): 
+                if (4 <= x <= 6): 
+                    return -1 * ((1 - (x-5)**2)**0.5)+1.7
+            elif 0 <= x <= 5: 
+                return 0.5 * exp(x+5)
+            elif 5 <= x <= 10: 
+                return 0.5 * exp(-x + 5)
+            else:
+                return -50
+                '''
+    return -50
+
+def slope (x): 
+    dx = 0.01
+    return (path (x + dx) - path (x-dx))/(2*dx)
+
+#def setup ():
+spawn_x = x_min + marble_r
+m = slope(spawn_x)
+n_mag = sqrt(1 + m**2)
+spawn_n = vector(-m / n_mag, 1 / n_mag, 0)
+marble_location = vector (spawn_x, path(spawn_x), 0) + (spawn_n * marble_r)
+marble = sphere (pos = marble_location, radius = marble_r, texture = textures.earth)
+marble.v = vector (20,0,0)
+
+running = False
+v_arrow = arrow (pos = marble.pos, axis = marble.v, color = color.yellow, shaftwidth = 0.3)
+v_temp = vector(20,0,0)
+
+
+def launch(): 
+    global running
+    running = True
+    v_arrow.visible = False
+
+button (text = "LAUNCH", bind = launch)
+
+
+def clicked(myevt):
+    global running
+    if running:
+        return
+    
+    click_pos = myevt.pos
+    speed = click_pos-marble.pos
+    v_arrow.pos = marble.pos
+    v_arrow.axis = speed
+    v_arrow.visible = True
+    marble.v = speed
+    
+scene.bind('click', clicked)
+
+path_pts = []
+step = 0.5
+for xcoord in arange(x_min, x_max+step, step):
+    path_pts.append(vector(xcoord, path(xcoord), 0))
+path_curve = curve(pos=path_pts, color=color.cyan, radius=0.2)
+
+def set_curves():
+    reset("curves")
+def set_loop():
+    reset("loop")
+def set_ramp():
+    reset("ramp")
+    
+button(text = "curve", bind=set_curves)
+button(text = "loop", bind=set_loop)
+button(text = "ramp", bind=set_ramp)
+
+def reset(path_new):
+    global path_type, t, running, marble_r
+    
+    path_type = path_new
+    path_curve.clear()
+    for xcoord in arange(x_min, x_max+step, step):
+        path_curve.append(vector(xcoord, path(xcoord), 0))
+    t=0
+    spawn_x = x_min + marble_r
+    m = slope(spawn_x)
+    n_mag = sqrt(1 + m**2)
+    spawn_n = vector(-m / n_mag, 1 / n_mag, 0)
+    marble.v = vector (20,0,0)
+    marble.pos = vector (spawn_x, path(spawn_x), 0) + (spawn_n * marble_r)
+
+omega = 0
+while True: 
+    rate (fps) # run 100 frames per sec
+    if running: 
+        if(marble.pos.y <= -49):
+            #print("help")
+            marble.v = vector(0,0,0)
+        F_net = vector(0,-g*mass,0)
+        #marble.pos.y = v_i*t + 0.5 * -9.81 * t** 2
+        if marble.pos.y <= (path (marble.pos.x)+marble_r): 
+            #path normal vector
+            m= slope(marble.pos.x)
+            n_mag= sqrt(1+m**2)
+            normal= vector(-m/ n_mag, 1/ n_mag, 0)
+            tangent = vector(1/n_mag, m/n_mag, 0)
+            v_t= dot(marble.v, tangent)
+            v_n = dot(marble.v, normal)
+            omega = v_t/marble_r
+            
+            if v_n < 0:
+                marble.pos.y = path(marble.pos.x)+marble_r
+                marble.v -= normal*v_n
+            
+            F_n = normal * (-dot(F_net, normal))
+            F_f = F_n * k #friction AUDREY AUDREY HELP!
+            F_net = F_net + F_n + F_f            
+        else:
+            omega = marble.v.x / marble_r
+            
+        a = F_net/mass
+        marble.v += a*dt
+        marble.pos += marble.v *dt
+            
+            
+        t += dt
+        marble.rotate (angle = -omega * dt, axis = vector (0, 0, 1)) #angle = radiuns turned per frame
+        #in this case angle = radians turned per 1/100 second
+        #with angular vel = 0.05 * 100 = 5 rad/s
