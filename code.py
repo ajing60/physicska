5cff8ef154f061e117d4d8691f793226d288d309
kwu61 <kwu61@stuy.edu>
2026-06-13 03:09:06 +0000

    i added some new paths

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
 Web VPython 3.2
-#define size of viewing box
 x_min = -80
 x_max = 80
 fps = 30
 t = 0
 dt = 1/fps
-g = 9.81 #14.81
-#making buttons for path type
-path_type = "curves"#"ramp""curves" #loop
+g = 9.81
+path_type = "Sine"
 mass= 10
 path_curve = None
+#marble_r=4
 marble_r=pow(mass, 1/3)
 k = 0.0
 k_static = k*1.3
-elasticity = 0.6
+elasticity = 0.0
 
 #assume marble is slolid spehre -> i=2/5 mr^2
 
@@ -41,11 +40,11 @@ wtext (text="1")
 scene.append_to_caption("\n\n")
 
 def path (x):
-    if path_type == "ramp": 
+    if path_type == "Ramp Down": 
         if x_min<x < 0: return 10
         if 0 <= x <= 30: return 10 - (0.5 * x)
         return -50
-    elif path_type == "curves":
+    elif path_type == "Sine":
         if  x_min<= x <= x_max: 
             return 4 * sin (x/10)
         else: return -50
@@ -64,26 +63,17 @@ def path (x):
         #    return 0.5 * exp(-x + 5)
     #    else:
     #        return -50
-    elif path_type == "loop":
+    elif path_type == "Free Fall":
         if  x_min<=x<0:
             return 0.03*(x+20)**2-40
-        #elif 0<=x<=80:
-        #    return -29
+    elif path_type == "Ramp Fancy":
+        if x_min<x < -40: return 10
+        if -40 <= x < -17: return (0.03*(x+40)**2) +10
+        if -17<=x<0: return (-2*x)-10
+        if 0<=x<50: return (-0.5*x) -10
+        return -50
         
-        '''
-        if (0.9 <= marble.pos.y <= 1.7): 
-            if (4 <= x <= 4.4 or 5.6 <= x <= 6): 
-                return ((1 - (x-5)**2)**0.5)+1.7
-            elif (1.7 < marble.pos.y): 
-                if (4 <= x <= 6): 
-                    return -1 * ((1 - (x-5)**2)**0.5)+1.7
-            elif 0 <= x <= 5: 
-                return 0.5 * exp(x+5)
-            elif 5 <= x <= 10: 
-                return 0.5 * exp(-x + 5)
-            else:
-                return -50
-                '''
+   
     return -50
 
 def slope (x): 
@@ -116,7 +106,9 @@ def launch():
     global running
     running = True
     v_arrow.visible = False
-
+    
+    
+scene.append_to_caption ("\n\t")
 button (text = "LAUNCH", bind = launch, color = color.green)
 
 
@@ -140,12 +132,14 @@ for xcoord in arange(x_min, x_max+step, step):
     path_pts.append(vector(xcoord, path(xcoord), 0))
 path_curve = curve(pos=path_pts, color=color.cyan, radius=0.2)
 
-def set_curves():
-    reset("curves")
-def set_loop():
-    reset("loop")
-def set_ramp():
-    reset("ramp")
+def set_Sine():
+    reset("Sine")
+def set_free():
+    reset("Free Fall")
+def set_down():
+    reset("Ramp Down")
+def set_up():
+    reset("Ramp Fancy")
     
 energy_graph = graph (title = "Energy Graph", xtitle = "time (sec)", ytitle = "energy (J)", width = 450, height = 250, align = "left")
 potential_curve = gcurve (graph = energy_graph, color = color.blue, label = "potential energy")
@@ -172,6 +166,10 @@ def reset(path_new):
     spawn_n = vector(-m / n_mag, 1 / n_mag, 0)
     marble.v = vector (20,0,0)
     marble.pos = vector (spawn_x, path(spawn_x), 0) + (spawn_n * marble_r)
+    
+    v_arrow.pos=marble.pos
+    v_arrow.axis=marble.v
+    
     potential_curve.data = []
     kinetic_curve.data = []
     velocity_curve.data = []
@@ -192,15 +190,25 @@ def self_reset():
     spawn_n = vector(-m / n_mag, 1 / n_mag, 0)
     marble.v = vector (20,0,0)
     marble.pos = vector (spawn_x, path(spawn_x), 0) + (spawn_n * marble_r)
+    
+    v_arrow.pos=marble.pos
+    v_arrow.axis=marble.v
+    
     potential_curve.data = []
     kinetic_curve.data = []
     velocity_curve.data = []
     normal_curve.data = []
 
 button (text = "RESET", bind = self_reset,color = color.red)
-button(text = "curve", bind=set_curves)
-button(text = "loop", bind=set_loop)
-button(text = "ramp", bind=set_ramp)
+
+scene.append_to_caption ("\n\t")
+scene.append_to_caption ("\n\t")
+scene.append_to_caption("\t<b>Path Types</b>\n")
+
+button(text = "Sine", bind=set_Sine)
+button(text = "Free Fall", bind=set_free)
+button(text = "Ramp Down", bind=set_down)
+button(text = "Ramp Fancy", bind=set_up)
 
 omega = 0
 F_n_mag = 0
@@ -221,12 +229,8 @@ while True:
             tangent = vector(1/n_mag, m/n_mag, 0)
             v_t= dot(marble.v, tangent) #along path/tangent
             v_n = dot(marble.v, normal)#normal
-            
-            ##ALLERRTTTT 
+             
             if v_n < 0:
-                #marble.pos = vector(marble.pos.x,path(marble.pos.x)+marble_r, 0) - audrey version
-            
-                marble.pos += normal * (path(marble.pos.x) + marble_r - marble.pos.y) * (1 / normal.y)
                 marble.pos.y = path(marble.pos.x) + marble_r
                 marble.v -= normal*v_n * (1 + elasticity) #ratio between velocity
                 v_t = dot(marble.v, tangent)
@@ -242,7 +246,7 @@ while True:
                 v_diff=v_t-(omega*marble_r) #difference? between translationan and rotational surface speed
                 #v_diff = 0 roll without slip
                 
-                if(abs(v_diff) > 0.01):#can change num? if there is a significant difference 
+                if(abs(v_diff) > 0.01):#if there is a significant difference 
                     if(v_diff > 0): #slipping kinetic fric
                         F_f = -tangent*k*F_n_mag
                     else: #rolling
@@ -264,7 +268,7 @@ while True:
                         F_f = tangent*static_max #they see me rollinnn
                         omega=v_t/marble_r
                         F_net = F_net + F_n + F_f
-                        a = F_net / (mass * (7.0 / 5.0)) #factor in rotational inertia 7/5
+                        a = F_net / (mass * (7/5)) #factor in rotational inertia 7/5
                         
                     else: #becomes kinetic
                         if v_diff>0:
@@ -277,13 +281,12 @@ while True:
                         alpha= torque /I
                         omega+= alpha * dt
                         F_net = F_net + F_n + F_f
-                        a = F_net / mass
-                        
+                        a = F_net /mass  
             else: #left the surface
                 F_n_mag = 0
                 F_n = vector(0, 0, 0)
                 F_f = vector(0, 0, 0)
-                a = F_net / mass
+                a = F_net/mass
                 
         else: #not on surface
             F_n_mag = 0
